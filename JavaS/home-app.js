f5bcc5d7e7edd3bcac43b7b8bded8953639c9fcc
raedeid <raededialrfou12345@gmail.com>
2019-12-02 09:45:04 +0200

    make another dynamic page

--- JavaS/home-app.js
+++ JavaS/home-app.js
@@ -1,4 +1,4 @@
-// 'use strict'
+'use strict'
 
 /* Open */
 function openNav() {
@@ -9,82 +9,103 @@ function openNav() {
 function closeNav() {
   document.getElementById("myNav").style.height = "0%";
 }
-var allBooks = [['Murder in Cherry Hills', 'mystery', '6-12', '2011-until now'],
-            ['Infinity', 'mystery', '13-18', '1990-2000'],
-            ['No Shelter', 'mystery', '19 or More', '2000-2010'],
-            ['Sindi and the Moon', 'true_story', '6-12', '2000-2010'],
-            ['The Girl with No Name', 'true_story', '13-18', '1990-2000'],
-            ['When Johnny Comes Marching', 'true_story', '19 or More', '2011-until now'],
-            ['The Brain: The Story of You', 'science', '13-18', '2011-until now'],
-            ['Audrey the Amazing Inventor', 'science', '6-12', '1990-2000'],
-            ['science encyclopedia', 'science', '19 or More', '2000-2010'],
-            ['Public Speaking Super Powers', 'ideas', '6-12', '1990-2000'],
-            ['Cofounding the Right Way', 'ideas', '13-18', '2000-2010'],
-            ['Social Intelligence', 'ideas', '19 or More', '2011-until now']];
 
-////////////////
-// Sticky Header
+
+
+
+
+var allBooks = [['Murder in Cherry Hills', 'mystery', '6-12', '2011-until now'],
+['Infinity', 'mystery', '13-18', '1990-2000'],
+['No Shelter', 'mystery', '19 or More', '2001-2010'],
+['Sindi and the Moon', 'true_story', '6-12', '2001-2010'],
+['The Girl with No Name', 'true_story', '13-18', '1990-2000'],
+['When Johnny Comes Marching', 'true_story', '19 or More', '2011-until now'],
+['The Brain: The Story of You', 'science', '13-18', '2011-until now'],
+['Audrey the Amazing Inventor', 'science', '6-12', '1990-2000'],
+['science encyclopedia', 'science', '19 or More', '2001-2010'],
+['Public Speaking Super Powers', 'ideas', '6-12', '1990-2000'],
+['Cofounding the Right Way', 'ideas', '13-18', '2001-2010'],
+['Social Intelligence', 'ideas', '19 or More', '2011-until now']];
+
+
 function Book(Name, Topic, Age, Date_write) {
   this.Name = Name;
   this.Topic = Topic;
   this.Age = Age;
   this.Date_write = Date_write;
   Book.all.push(this);
-  
+
 }
 Book.all = [];
 for (var z = 0; z < allBooks.length; z++) {
- new Book(allBooks[z][0],allBooks[z][1],allBooks[z][2],allBooks[z][3]);
+  new Book(allBooks[z][0], allBooks[z][1], allBooks[z][2], allBooks[z][3]);
 }
 
-var request = [];
-var myFunction = function (event) {
-  request = []
-  if (event.target.option.id == 'mystery') {
-    request.push('mystery')
+// var request = [];
 
-  }else if (event.target.option.id == "true_story") {
+var pick = document.getElementById('form');
+var topic = document.getElementById("chooseTopic");
+console.log(Book.all)
+var date = document.getElementById('chooseDate');
+var age = document.getElementById('targetAge');
+pick.addEventListener('submit', function (event) {
+  event.preventDefault();
+  var request = [];
+  // alert('hi')
+
+  if (topic.value === 'mystery') {
+    console.log("hi")
+    request.push('mystery')
+  } else if (topic.value === "true_story") {
     request.push('true_story')
-  }else if (event.target.option.id == "science") {
-    request.push('novels');
-  }else if (event.target.option.id == "ideas") {
+  } else if (topic.value === "sience") {
+    request.push('sience');
+  } else if (topic.value === "ideas") {
     request.push('ideas');
   }
-  if (event.target.option.id == "1990-2000") {
-    request.push('1990-2000');
-  }
-  if (event.target.option.id == "2000-2010") {
-    request.push('2000-2010');
-  }
-  if (event.target.option.id == "2011-until now") {
-    request.push('2011-until now')
-  }
-
-  if (event.target.option.id == "6-12") {
+  // console.log(request)
+  if (age.value === "6-12") {
     request.push('6-12');
 
-  }
-  if (event.target.option.id == "13-18") {
+  } else if (age.value === "13-18") {
     request.push('13-18');
 
-  }
-  if (event.target.option.id == "19 or More") {
+  } else if (age.value === "19 or More") {
     request.push('19 or More');
-
   }
 
-}
-var pick = document.getElementById('submit');
-pick.addEventListener('submit', myFunction);
-console.log(pick);
-// console.log(request);
-// var local_storage_book = JSON.stringify(localStorage.setItem('request_customer',request))
-var books_request = []
-for (var i=0;i<request.length;i++){
-  for (var l=0;l<Book.all.length;l++){
-    if (request[i] === Book.all[l].Topic){
-
-      
+  if (date.value === "1990-2000") {
+    request.push('1990-2000');
+  } else if (date.value === "2001-2010") {
+    request.push('2001-2010');
+  } else if (date.value === "2011-until now") {
+    request.push('2011-until now')
+  }
+  // console.log(request);
+  // console.log(Book.all);
+  // console.log(pick);
+  var books_request = []
+  function isEqual(obj) {
+    console.log(request);
+    if (request[0] === obj.Topic) {
+      if (request[1] === obj.Age) {
+        if (request[2] === obj.Date_write) {
+          return true
+        }
+      }
     }
-}
-}
+    return false
+  }
+  for (var i=0;i<Book.all.length;i++){
+    if (isEqual(Book.all[i])){
+      books_request.push(Book.all[i]);
+    }
+  }
+  console.log(books_request);
+  var local_storage_book = localStorage.setItem('request_customer',JSON.stringify(books_request));
+  window.location.href = "/shop.html"
+
+});
+
+
+
--- JavaS/shop.js
+++ JavaS/shop.js
@@ -1,2 +1,35 @@
-var call_local_storage = JSON.parse(localStorage.getItem( 'request_customer'))
-console.log(call_local_storage);
+// var feed_back = JSON.parse(localStorage.getItem('request_customer'));
+var feed_back = [{Name: "Murder in Cherry Hills", Topic: "mystery", Age: "6-12", Date_write: "2011-until now",Image_path:'img/Lead-nurturing-optimieren-header.png',Price:'5Jd'},
+                {Name: "Infinity", Topic: "mystery", Age: "13-18", Date_write: "1990-2000",Image_path:'img/Naysayers.jpg',Price:'10jd'}]
+var body = document.getElementById('body');
+var add_cart = localStorage.setItem('cart',[])
+var collection_of_cart = []
+for (var w=0;w<feed_back.length;w++){
+    var create_div = document.createElement('div')
+    create_div.setAttribute('class',"book-section");
+    body.appendChild(create_div);
+    var create_image = document.createElement('img')
+    create_image.setAttribute('src',feed_back[w].Image_path);
+    create_div.appendChild(create_image);
+    var create_span = document.createElement('span');
+    create_span.textContent = feed_back[w].Price ;
+    create_div.appendChild(create_span);
+    create_button = document.createElement('button')
+    create_button.setAttribute('type','submit');
+    create_button.setAttribute('class','take');
+    create_button.textContent = 'add to cart'
+    create_div.appendChild(create_button);
+    create_button.setAttribute('onclick','MyFunction()')
+    console.log(collection_of_cart)
+    // var listen_to = document.getElementById('take');
+    // console.log(listen_to);
+    // listen_to.addEventListener('submit',function(event){
+    //     alert('hi');
+    //     console.log(add_cart)
+
+}
+var MyFunction = function(event){
+    localStorage.setItem('cart',JSON.stringify(feed_back[w]))
+        
+        
+}
