99f52b443f80090abbcb830f9ff131514741bced
raedeid <raededialrfou12345@gmail.com>
2019-12-03 16:30:45 +0200

    depugg

--- JavaS/shop.js
+++ JavaS/shop.js
@@ -1,37 +1,36 @@
 var feed_back = JSON.parse(localStorage.getItem('request_customer'));
 console.log(feed_back);
 var body = document.getElementById('body');
-var add_cart = localStorage.setItem('cart',[])
+// var add_cart = localStorage.setItem('cart', '')
 var collection_of_cart = []
 var create_div = document.createElement('div')
-function render(){
-    for (var w=0;w<feed_back.length;w++){
-        create_div = document.createElement('div')
-        create_div.setAttribute('class',"book-section");
-        body.appendChild(create_div);
-        var create_image = document.createElement('img');
-        create_image.setAttribute('src',feed_back[w].Image_path);
-        create_image.setAttribute('class','rss');
-        create_div.appendChild(create_image);
-        var create_another_div = document.createElement('div');
-        create_another_div.textContent = feed_back[w].Price ;
-        create_another_div.setAttribute('class','price')
-        create_div.appendChild(create_another_div);
-        create_button = document.createElement('button');
-        create_button.setAttribute('type','submit');
-        create_button.setAttribute('class','take');
-        create_button.textContent = 'add to cart'
-        create_div.appendChild(create_button);
-        create_div.setAttribute('class','feed');
-        create_button.setAttribute('onclick','increase()')
-        console.log(collection_of_cart)
-        // var listen_to = document.getElementById('take');
-        // console.log(listen_to);
-        // listen_to.addEventListener('submit',function(event){
-        //     alert('hi');
-        //     console.log(add_cart)
-    
-    }
+function render() {
+    create_div = document.createElement('div')
+    create_div.setAttribute('class', "book-section");
+    body.appendChild(create_div);
+    var create_image = document.createElement('img');
+    create_image.setAttribute('src', feed_back[0].Image_path);
+    create_image.setAttribute('class', 'rss');
+    create_div.appendChild(create_image);
+    var create_another_div = document.createElement('div');
+    create_another_div.textContent = feed_back[0].Price;
+    create_another_div.setAttribute('class', 'price')
+    create_div.appendChild(create_another_div);
+    create_button = document.createElement('button');
+    create_button.setAttribute('type', 'submit');
+    create_button.setAttribute('class', 'take');
+    create_button.textContent = 'add to cart'
+    create_div.appendChild(create_button);
+    create_div.setAttribute('class', 'feed');
+    create_button.setAttribute('onclick', 'increase()')
+    console.log(collection_of_cart)
+    // var listen_to = document.getElementById('take');
+    // console.log(listen_to);
+    // listen_to.addEventListener('submit',function(event){
+    //     alert('hi');
+    //     console.log(add_cart)
+
+
 }
 render();
 
@@ -57,32 +56,40 @@ render();
 //     create_element.appendChild(create_number);
 //     localStorage.setItem('cart',JSON.stringify(feed_back))
 
-             
+
 // }
+// var update = document.getElementById('number')
 // var bottom_div = document.createElement('div')
+// update.textContent = feed_back[0].Quantity 
 // body.appendChild(bottom_div)
-var increase = function (){
-    feed_back[0].Quantity ++
-    // var update = document.getElementById('number')
-    // update.textContent = feed_back[0].Quantity 
-    localStorage.setItem('cart',JSON.stringify(feed_back[0]))
-    // bottom_div.textContent = `The total cost of books ${feed_back[0].Quantity} * ${feed_back[0].Price} = ${feed_back[0].Quantity * parseInt(feed_back[0].Price)} Jd`
+// bottom_div.textContent = `The total cost of books ${feed_back[0].Quantity} * ${feed_back[0].Price} = ${feed_back[0].Quantity * parseInt(feed_back[0].Price)} Jd`
+var increase = function () {
+    if (JSON.parse(localStorage.getItem('cart')) == null) {
+        feed_back[0].Quantity++
+        localStorage.setItem('cart', JSON.stringify(feed_back[0]))
+        
+    } else {
+        var update = JSON.parse(localStorage.getItem('cart'))
+        update.Quantity++
+        localStorage.setItem('cart',JSON.stringify(update))
+    }
+
 }
-var decrease = function(){
-    if (feed_back[0].Quantity > 0){
+var decrease = function () {
+    if (feed_back[0].Quantity > 0) {
         var update = document.getElementById('number')
-        feed_back[0].Quantity --
+        feed_back[0].Quantity--
         update.textContent = feed_back[0].Quantity
-        localStorage.setItem('cart',JSON.stringify(feed_back[0]))
+        localStorage.setItem('cart', JSON.stringify(feed_back[0]))
 
     }
 
 }
-var go = function (){
+var go = function () {
     window.location.href = '/result.html'
 }
 var create_finish = document.createElement('button')
-create_finish.setAttribute('type','submit');
-create_finish.setAttribute('onclick','go()')
+create_finish.setAttribute('type', 'submit');
+create_finish.setAttribute('onclick', 'go()')
 body.appendChild(create_finish);
 create_finish.textContent = 'Cart'
