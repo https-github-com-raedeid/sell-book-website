633f04d2e5a4861e97fa1f1cd449c4b3ffac0d0e
alaamahasneh <alaa.ibraheem97@yahoo.com>
2019-12-03 15:14:30 +0200

    markdow page

--- JavaS/thanks.js
+++ JavaS/thanks.js
@@ -5,4 +5,4 @@ var createP= document.createElement('p');
 createP.setAttribute("class","text");
 var bodyEl= document.getElementById('bodyEl');
 bodyEl.appendChild(createP);
-createP.textContent = `Thanks    ${nameCustomer}   to visit our web sit , you will get your order soon`;
+createP.textContent = `Thanks    ${nameCustomer}   to visit our web site , you will get your order soon`;
